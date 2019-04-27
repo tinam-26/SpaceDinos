4d9c57702062f568e3d6f32a0af62db394626464
Tina <18502544+tinam-26@users.noreply.github.com>
2019-04-27 17:02:16 -0400

    intro animation + switches to main scene

---
+++ Assets/screenController1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenController1 : MonoBehaviour
+{
+    public GameObject thisScene;
+    public GameObject nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("clicked");
+            thisScene.SetActive(false);
+            nextScene.SetActive(true);
+        }
+    }
+}
+
+ 
