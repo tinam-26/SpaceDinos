3e939ad1631aa5585c9c15db1b756fdb5506a071
Jordan McLeod <jordanmcleod@example.com>
2019-04-28 12:26:08 -0400

    Fix background overlap bug

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.IO;
 
 public class ObjectSpawner : MonoBehaviour
@@ -10,6 +11,8 @@ public class ObjectSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(level == 3)
+        SceneManager.LoadScene("introScene", LoadSceneMode.Single);
         SpawnFromJson(System.IO.File.ReadAllText($"{Application.streamingAssetsPath}/levels/{level}.json"));
     }
 
