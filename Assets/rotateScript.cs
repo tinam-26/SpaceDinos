8245079ec5bcef9a26f40b14afe12656ecfbe0b4
Tina <18502544+tinam-26@users.noreply.github.com>
2019-04-27 22:56:22 -0400

    menuScene + planetRotation

---
+++ Assets/rotateScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateScript : MonoBehaviour
+{
+    public GameObject self;
+    public float xAngle, yAngle, zAngle; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate on z axis
+        //self.transform.position = new Vector3(0.75f, 0.0f, 0.0f);
+        self.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+    }
+}
